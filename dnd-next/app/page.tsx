0f0c9cb00cbf95ffe817d59720aa167f8388b0f7
kvanderlyn <kvanderlyn@ttsiglobal.com>
2023-03-12 22:59:40 -0400

    Add logo and change homepage

--- dnd-next/app/page.tsx
+++ dnd-next/app/page.tsx
@@ -1,11 +1,60 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from './page.module.css';
+import bgImage from '../public/images/dungeons-deep.jpg';
+import logo from '../public/logos/stacked_white.svg';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div>I am the homepage</div>
-  )
+    <div className="h-screen w-screen bg-gray-900 overflow-auto">
+      <div className="w-full relative">
+        <Image
+          alt="Adventurer looks to the horizon where dragons circle the ancient keep"
+          src={bgImage}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          className="bg-center z-0"
+          style={{
+            objectFit: 'cover'
+          }}
+        />
+        <div className="relative px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-white">Hello World</div>
+
+          <div className="mx-auto max-w-2xl py-32 sm:py-64 lg:py-56">
+            <div>
+              <Image alt="Logo" height={150} src={logo} quality={100} />
+              <p className="px-2 py-1 mt-6 text-lg leading-8 text-gray-300 ">
+                <span className="bg-black p-2">
+                  A new way to manage your characters and campaigns.
+                </span>
+                <br />
+                <span className="bg-black p-2">
+                  Your books, your game, your way.
+                </span>
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md border-2 border-orange-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-300"
+                >
+                  Get started
+                </a>
+                <a
+                  href="#about"
+                  className="text-base border-b-2 border-transparent font-semibold leading-7 text-gray-100 hover:border-white"
+                >
+                  Learn more <span aria-hidden="true">🠣</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
